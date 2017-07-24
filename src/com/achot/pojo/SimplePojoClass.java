7f4d68329249415df9c6c9eab7945a091aca9d90
Angel Tringov <atringov@sapient.com>
2017-07-24 14:41:53 +0300

    Add first class

---
+++ src/com/achot/pojo/SimplePojoClass.java
@@ -0,0 +1,16 @@
+package com.achot.pojo;
+
+/**
+ * Created by Angel.Tringov on 24.7.2017 г..
+ */
+public class SimplePojoClass {
+    private String simple;
+
+    public String getSimple() {
+        return simple;
+    }
+
+    public void setSimple(String simple) {
+        this.simple = simple;
+    }
+}
